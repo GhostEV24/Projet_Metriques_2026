a5084f45eda23474c3b10012702ddf90765535ec
bstocker <boris.stocker@gmail.com>
2026-02-15 17:48:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,14 @@ app = Flask(__name__)
 def hello_world():
     return render_template('hello.html')
 
-@app.get("/health")
-def health():
-    return jsonify(status="ok")
+# Déposez votre code à partir d'ici :
+
+
+
+
+
+
+# Ne rien mettre après ce commentaire
     
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=5000, debug=True)
