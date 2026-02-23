1edeaeb87dddc5a00129499343547c759e89831c
GhostEV24 <168532841+GhostEV24@users.noreply.github.com>
2026-02-23 15:14:08 +0000

    Atelier Reykjavik ComboChart

--- app.py
+++ app.py
@@ -8,10 +8,68 @@ def hello_world():
     return render_template('hello.html')
 
 # Déposez votre code à partir d'ici :
+@app.route("/contact")
+def MaPremiereAPI():
+    return render_template("contact.html") 
 
 
+@app.get("/paris")
+def api_paris():
+    
+    url = "https://api.open-meteo.com/v1/forecast?latitude=48.8566&longitude=2.3522&hourly=temperature_2m"
+    response = requests.get(url)
+    data = response.json()
+
+    times = data.get("hourly", {}).get("time", [])
+    temps = data.get("hourly", {}).get("temperature_2m", [])
+
+    n = min(len(times), len(temps))
+    result = [
+        {"datetime": times[i], "temperature_c": temps[i]}
+        for i in range(n)
+    ]
+
+    return jsonify(result)
+
+@app.route("/rapport")
+def mongraphique():
+    return render_template("graphic.html")
+
+@app.route("/histogramme")
+def histogramme():
+    return render_template("histo.html")
 
 
+@app.get("/atelier-data")
+def atelier_data():
+    import requests
+    from flask import jsonify
+
+    url = "https://api.open-meteo.com/v1/forecast?latitude=64.1466&longitude=-21.9426&hourly=temperature_2m,windspeed_10m"
+
+    response = requests.get(url)
+    data = response.json()
+
+    times = data.get("hourly", {}).get("time", [])
+    temps = data.get("hourly", {}).get("temperature_2m", [])
+    winds = data.get("hourly", {}).get("windspeed_10m", [])
+
+    n = min(len(times), len(temps), len(winds))
+
+    result = [
+        {
+            "datetime": times[i],
+            "temperature": temps[i],
+            "wind": winds[i]
+        }
+        for i in range(n)
+    ]
+
+    return jsonify(result)
+
+@app.route("/atelier")
+def atelier():
+    return render_template("atelier.html")
 
 
 # Ne rien mettre après ce commentaire
